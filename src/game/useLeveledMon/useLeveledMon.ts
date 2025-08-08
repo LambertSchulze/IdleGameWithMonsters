d20dafe72697678aa27a5f4a8df1f636d058572a
LambertSchulze <schulze.lambert@googlemail.com>
2025-08-08 21:59:06 +0200

    Refactor: rename function name

--- src/game/useLeveledMon/useLeveledMon.ts
+++ src/game/useLeveledMon/useLeveledMon.ts
@@ -2,7 +2,7 @@ import { skipToken } from '@reduxjs/toolkit/query'
 import type { MonName, Stats } from '../../store/pokemonApi'
 import { useMonDetailQuery, useSpeciesDetailQuery } from '../../store/pokemonApi'
 import { getGrowthRate } from '../GrowthRate/getGrowthRate'
-import { getStats } from '../Stats/getStats'
+import { getLeveledStats } from '../Stats/getLeveledStats'
 
 export interface LeveledMon {
   stats: Stats
@@ -20,7 +20,7 @@ export const useLeveledMonQuery = (name: MonName, level: number): LeveledMon | u
 
   if (monDetail.isSuccess && speciesDetail.isSuccess && growthRate) {
     return {
-      stats: getStats(monDetail.data.baseStats, level),
+      stats: getLeveledStats(monDetail.data.baseStats, level),
       expForNextLvl: growthRate(level + 1) - growthRate(level)
     }
   }
